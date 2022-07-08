3343df944ae0f44607bbf1b271952c21ffc1194d
Daniel Jeon <djeon93@gmail.com>
2022-07-08 11:20:37 -0700

    added and finished header, added home

---
+++ eshop/src/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return (
+        <div>
+            <div className="home">
+                <div className="home__container">
+                    <img src="https://wscai.org/wp-content/uploads/2022/03/wscai-artwork-advertising-website.png" alt="" className="home__image" />
+                    <div className="home__row">
+
+                    </div>
+                    <div className="home__row">
+                        
+                    </div>
+                    <div className="home__row">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
